7433189043e323551bc8a1a5708114b9fcd29b57
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-02-04 20:23:14 +0100

    really evolving in this game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
@@ -7,7 +8,7 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit,AfterViewInit{
   title = 'graphic';
   @ViewChild('threejsContainer', { static: true }) threejsContainer: any;
   @ViewChild('div') div: any;
@@ -19,17 +20,34 @@ export class AppComponent {
   animationMixer:any
   animationAction :any
 clown:any
+trialForm!:FormGroup
+rotateX:number=0
+scale=0.0
+left='40%'
+i:number=40
   constructor() { }
+  ngAfterViewInit(): void {
+  //  this.div.nativeElement.classList.add('position-absolute','top-50','start-50','translate-middle')
+    this.div.nativeElement.style.zIndex=`4`
+    this.div.nativeElement.style.position='absolute'
+    this.div.nativeElement.style.left=this.left
+    this.div.nativeElement.style.scale=this.scale
+    // this.div.nativeElement.style.marginLeft=`45%`
+    // this.div.nativeElement.style.scale=`4`
+
+
+  }
 
   ngOnInit(): void {
+    this.trialForm=new FormGroup({})
     this.scene = new THREE.Scene();
     this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
     this.renderer = new THREE.WebGLRenderer({ canvas: this.threejsContainer.nativeElement ,antialias:true,alpha:true});
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     this.renderer.shadowMap.enabled = true;
     this.camera.position.x=0
-    this.camera.position.y=0.45
-    this.camera.position.z=-1
+    this.camera.position.y=0
+    this.camera.position.z=0
       const loader = new GLTFLoader();
     loader.load('assets//clown/scene.gltf', (gltf:any) => {
       this.gltfModel = gltf.scene;
@@ -37,7 +55,7 @@ clown:any
       const ambientLight1 = new THREE.AmbientLight(0xffffff, 1);
       const directionalLight1 = new THREE.DirectionalLight(0xffffff, 15);
       this.scene.add(this.gltfModel,ambientLight1,directionalLight1);
-      this.gltfModel.rotation.y+=1
+      this.gltfModel.position.set(0,-0.2,0)
       this.clown=this.gltfModel
       if (gltf.animations && gltf.animations.length > 0) {
         this.animationMixer = new THREE.AnimationMixer(this.gltfModel);
@@ -68,38 +86,46 @@ clown:any
 
     this.renderer.render(this.scene, this.camera);
   }
-  marginLeft=0;
-  scale=3
-  rotateY=0
+
   @HostListener('document:wheel', ['$event'])
   public onWheel(targetElement:any) {
-console.log(  this.div.nativeElement.style)
 
       if(targetElement.deltaY>0){
-        if(this.camera.position.z<0.99){
-          this.marginLeft+=100
-this.scale-=0.1
-this.rotateY+=0.1
-        this.camera.position.z+=0.1
-        this.div.nativeElement.style.marginLeft=`${this.marginLeft}px`
-        this.div.nativeElement.style.scale=this.scale
-        this.div.nativeElement.style.transform=`rotateY(${this.rotateY})`
+        if(this.rotateX<=320){
+           this.rotateX+=40
+        }
+
+        if(this.i<=60){
+this.i+=2
+this.left=`${this.i}%`
+this.div.nativeElement.style.left=this.left
 
         }
-        if(this.camera.position.y>0.14999999999999983){
-        this.camera.position.y-=0.025
-      }
+
+        if(this.scale<1.1){
+          this.scale+=0.2
+        this.div.nativeElement.style.scale=this.scale
+        }
+
+        this.div.nativeElement.style.transform = `rotateY(${this.rotateX}deg)`;
+        if(this.camera.position.z<0.69){
+        this.camera.position.z+=0.1
+        }
         this.gltfModel.rotation.y-=0.5
       }else{
+
         this.camera.position.z-=0.1
-        this.marginLeft-=100
-        this.div.nativeElement.style.marginLeft=`${this.marginLeft}px`
-        if(this.camera.position.z<0.89){
-        this.camera.position.y+=0.025
-        }
         this.gltfModel.rotation.y+=0.5
       }
   }
+  @HostListener('window:resize', ['$event'])
+  public onWindowResize(){
 
+      this.camera.aspect = window.innerWidth / window.innerHeight;
+      this.camera.updateProjectionMatrix();
+
+      this.renderer.setSize( window.innerWidth, window.innerHeight );
+
+  }
 
 }
