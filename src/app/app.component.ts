21609e27ec6a14f467899386eeadbbfffc704dc7
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-02-05 06:55:26 +0100

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,19 +31,20 @@ i:number=20
   ngAfterViewInit(): void {
   //  this.div.nativeElement.classList.add('position-absolute','top-50','start-50','translate-middle')
     this.div.nativeElement.style.zIndex=`4`
-    this.div.nativeElement.style.position='absolute'
+    this.div.nativeElement.classList.add('position-absolute')
     this.div.nativeElement.style.left=this.left
     this.div.nativeElement.style.scale=this.scale
+    this.div.nativeElement.style.top='10%'
     this.div1.nativeElement.style.zIndex=`4`
-    this.div1.nativeElement.style.position='absolute'
+    this.div1.nativeElement.style.top='10%'
+    this.div1.nativeElement.classList.add('position-absolute')
     this.div1.nativeElement.style.right=this.left
     this.div1.nativeElement.style.scale=this.scale
     this.div2.nativeElement.style.scale=this.scale
     this.div2.nativeElement.style.position='fixed'
     this.div2.nativeElement.style.top='0'
-    this.div.nativeElement.style.top='10%'
-    this.div1.nativeElement.style.top='10%'
     this.div2.nativeElement.style.left='45%'
+    this.div2.nativeElement.style.zIndex=`4`
   }
 
   ngOnInit(): void {
@@ -52,9 +53,10 @@ i:number=20
     this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
     this.renderer = new THREE.WebGLRenderer({ canvas: this.threejsContainer.nativeElement ,antialias:true,alpha:true});
     this.renderer.setSize(window.innerWidth, window.innerHeight);
-    this.renderer.shadowMap.enabled = true;
-    this.camera.position.x=0
-    this.camera.position.y=0
+
+    this.renderer.domElement.style.background = 'linear-gradient(to top right, #e74c3c, #ffffff)';
+
+        this.camera.position.y=0
     this.camera.position.z=0
       const loader = new GLTFLoader();
     loader.load('assets//clown/scene.gltf', (gltf:any) => {
@@ -111,13 +113,13 @@ this.div1.nativeElement.style.right=this.left
         }
 
         if(this.scale<1.1){
-          this.scale+=0.2
+          this.scale+=0.1
         this.div.nativeElement.style.scale=this.scale
         this.div1.nativeElement.style.scale=this.scale
+this.div2.nativeElement.style.scale=this.scale
 
       }
 
-this.div2.nativeElement.style.scale=this.scale
         this.div.nativeElement.style.transform = `rotateY(${this.rotateX}deg)`;
         this.div1.nativeElement.style.transform = `rotateY(${-this.rotateX}deg)`;
 
@@ -127,12 +129,38 @@ this.div2.nativeElement.style.scale=this.scale
         this.gltfModel.rotation.y-=0.5
       }else{
 
-        this.camera.position.z-=0.1
+
+        if(this.rotateX>=0){
+          this.rotateX-=40
+       }
+       if(this.i>=20){
+this.i-=3
+this.left=`${this.i}%`
+this.div.nativeElement.style.left=this.left
+this.div1.nativeElement.style.right=this.left
+
+       }
+
+       if(this.scale>0.1){
+         this.scale-=0.1
+         console.log(this.scale)
+       this.div.nativeElement.style.scale=this.scale
+       this.div1.nativeElement.style.scale=this.scale
+this.div2.nativeElement.style.scale=this.scale
+
+     }
+
+       this.div.nativeElement.style.transform = `rotateY(${this.rotateX}deg)`;
+       this.div1.nativeElement.style.transform = `rotateY(${-this.rotateX}deg)`;
+
+       if(this.camera.position.z>0){
+       this.camera.position.z-=0.1
+       }
         this.gltfModel.rotation.y+=0.5
       }
   }
   @HostListener('window:resize', ['$event'])
-  public onWindowResize(){
+  public onWindowResize(event:any){
 
       this.camera.aspect = window.innerWidth / window.innerHeight;
       this.camera.updateProjectionMatrix();
